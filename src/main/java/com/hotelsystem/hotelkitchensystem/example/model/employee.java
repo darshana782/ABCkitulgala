c4ab005e12d5f009b7395c34ae7147362a0d95f9
darshana782 <56575929+darshana782@users.noreply.github.com>
2021-07-19 01:56:14 +0530

    login -> fro+back (by type) employee uncomment

--- src/main/java/com/hotelsystem/hotelkitchensystem/example/model/employee.java
+++ src/main/java/com/hotelsystem/hotelkitchensystem/example/model/employee.java
@@ -1,25 +1,25 @@
-//package com.hotelsystem.hotelkitchensystem.example.model;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.*;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Entity
-//@Table(name = "Employee")
-//public class employee {
-//    @Id
-//    @GeneratedValue
-//    private int emp_id;
-//    private String f_name;
-//    private String l_name;
-//    @Column(columnDefinition = "VARCHAR")
-//    private String email;
-//    private int contact_no;
-//    private String gender;
-//    private String type;
-//}
+package com.hotelsystem.hotelkitchensystem.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Employee")
+public class employee {
+    @Id
+    @GeneratedValue
+    private int emp_id;
+    private String f_name;
+    private String l_name;
+    @Column(columnDefinition = "VARCHAR")
+    private String email;
+    private int contact_no;
+    private String gender;
+    private String type;
+}
