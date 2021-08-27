4596ab4655184ff502d404194cc73731a747aff2
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-08-28 00:46:10 +0530

    discount adding

---
+++ src/main/java/com/hotelsystem/hotelkitchensystem/example/controller/DiscountsController.java
@@ -0,0 +1,30 @@
+package com.hotelsystem.hotelkitchensystem.example.controller;
+
+import com.hotelsystem.hotelkitchensystem.example.dto.request.AddDiscountsRequest;
+import com.hotelsystem.hotelkitchensystem.example.service.RoomDiscountsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/manager")
+public class DiscountsController {
+    @Autowired
+    private RoomDiscountsService roomDiscountsService;
+
+    @PostMapping("/addDiscounts")
+    public ResponseEntity addDiscounts(@RequestBody AddDiscountsRequest addDiscountsRequest){
+        String FromDate = addDiscountsRequest.getFromDate();
+        String responseMsg;
+
+        if(roomDiscountsService.checkIfDiscountExists(FromDate)){
+            responseMsg = "Discount already Exists";
+        }else{
+            roomDiscountsService.addNewDiscounts(addDiscountsRequest);
+            responseMsg = "Successfully Added";
+            return ResponseEntity.ok().body(responseMsg);
+        }
+        return ResponseEntity.badRequest().body(responseMsg);
+    }
+}
