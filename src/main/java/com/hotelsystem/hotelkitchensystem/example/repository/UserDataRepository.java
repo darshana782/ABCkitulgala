ee6f84dce2159b0cf1c5f94c03cc0d30618bd3fa
Thusiya <56513558+Thusiya05@users.noreply.github.com>
2021-09-06 19:44:15 +0530

    bla

--- src/main/java/com/hotelsystem/hotelkitchensystem/example/repository/UserDataRepository.java
+++ src/main/java/com/hotelsystem/hotelkitchensystem/example/repository/UserDataRepository.java
@@ -3,6 +3,8 @@ package com.hotelsystem.hotelkitchensystem.example.repository;
 import com.hotelsystem.hotelkitchensystem.example.enums.UserType;
 import com.hotelsystem.hotelkitchensystem.example.model.UserData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,5 +15,8 @@ public interface UserDataRepository extends JpaRepository<UserData, Integer> {
 
     List<UserData> findByUserTypeNotAndDeleteStatus(UserType type, String status);
 
-    List<UserData> findAllByFirstNameIsContainingOrLastNameIsContainingOrContactNoIsContainingAndUserTypeNotAndDeleteStatus(String f_name, String l_name,String contact, UserType userType, String status);
+    List<UserData> findByFirstNameIsContainingAndUserTypeNotAndDeleteStatus(String f_name, UserType userType, String status);
+
+//    @Query(value= "select * from UserData_Table where firstName like ?1% or lastName like ?2% OR contactNo like ?3% and userType = ?4 and deleteStatus = ?5",nativeQuery = true)
+//    List<UserData> find ByTest(String f_name, String l_name, String contact, UserType type, String status);
 }
