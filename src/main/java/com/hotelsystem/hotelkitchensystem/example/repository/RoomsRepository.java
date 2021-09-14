d615dc7752133d911626b89a072051566491f7b3
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-09-14 16:24:00 +0530

    add customer

--- src/main/java/com/hotelsystem/hotelkitchensystem/example/repository/RoomsRepository.java
+++ src/main/java/com/hotelsystem/hotelkitchensystem/example/repository/RoomsRepository.java
@@ -1,10 +1,12 @@
 package com.hotelsystem.hotelkitchensystem.example.repository;
 
+import com.hotelsystem.hotelkitchensystem.example.enums.RoomTypes;
 import com.hotelsystem.hotelkitchensystem.example.model.Rooms;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomsRepository extends JpaRepository<Rooms,Integer> {
     Rooms findByRoomNo(int roomNo);
+    Rooms findByRoomType(RoomTypes roomTypes);
 
 //    Rooms findByRoomNo(int roomNo);
 }
