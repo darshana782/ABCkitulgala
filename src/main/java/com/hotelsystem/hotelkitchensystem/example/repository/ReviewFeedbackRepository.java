4c362dbb775bd3c71af71c3c54b846ad573f6726
Adithya1303 <56554121+Adithya1303@users.noreply.github.com>
2021-09-17 22:06:31 +0530

    Create ReviewFeedbackRepository.java

---
+++ src/main/java/com/hotelsystem/hotelkitchensystem/example/repository/ReviewFeedbackRepository.java
@@ -0,0 +1,7 @@
+package com.hotelsystem.hotelkitchensystem.example.repository;
+
+import com.hotelsystem.hotelkitchensystem.example.model.ReviewFeedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewFeedbackRepository extends JpaRepository<ReviewFeedback,Integer> {
+}
