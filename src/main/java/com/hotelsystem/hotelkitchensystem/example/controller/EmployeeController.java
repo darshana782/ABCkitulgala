075d0de04309f279195c79bcc90d8e7130edd131
Lakith Kithsara <52199455+LakithKithsara@users.noreply.github.com>
2021-09-21 08:11:04 +0530

    Update EmployeeController.java

--- src/main/java/com/hotelsystem/hotelkitchensystem/example/controller/EmployeeController.java
+++ src/main/java/com/hotelsystem/hotelkitchensystem/example/controller/EmployeeController.java
@@ -48,7 +48,7 @@ public class EmployeeController {
         final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         SecureRandom random = new SecureRandom();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 8; i++)
         {
             int randomIndex = random.nextInt(chars.length());
             sb.append(chars.charAt(randomIndex));
